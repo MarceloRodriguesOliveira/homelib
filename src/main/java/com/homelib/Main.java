0cda50c8e3d9e7268fa39461a9386be8a29a731e
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-04-28 11:59:21 -0300

    build: removed unused folders and enabled logging annotation

--- src/main/java/com/homelib/Main.java
+++ src/main/java/com/homelib/Main.java
@@ -1,7 +1,12 @@
 package com.homelib;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        log.info("Isso é um hello world padrão");
     }
 }
