91e84fbca89203b670509624bacddbd7dfbb6d2e
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-04-29 16:04:17 -0300

    chore: created builder for book class

---
+++ src/main/java/com/homelib/entities/Book.java
@@ -0,0 +1,72 @@
+package com.homelib.entities;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+    private String title;
+    private String firstNameAuthor;
+    private String lastNameAuthor;
+    private int year;
+    private int edition;
+
+    public Book(String name, String firstNameAuthor, String lastNameAuthor, int year, int edition) {
+        this.title = name;
+        this.firstNameAuthor = firstNameAuthor;
+        this.lastNameAuthor = lastNameAuthor;
+        this.year = year;
+        this.edition = edition;
+    }
+
+    public static final class BookBuilder{
+        private String title;
+        private String firstNameAuthor;
+        private String lastNameAuthor;
+        private int year;
+        private int edition;
+
+        public BookBuilder() {
+        }
+
+        public static BookBuilder builder(){
+            return new BookBuilder();
+        }
+
+        public BookBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public BookBuilder FirstNameAuthor(String firstNameAuthor) {
+            this.firstNameAuthor = firstNameAuthor;
+            return this;
+        }
+
+        public BookBuilder LastNameAuthor(String lastNameAuthor) {
+            this.lastNameAuthor = lastNameAuthor;
+            return this;
+        }
+
+        public BookBuilder Year(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public BookBuilder Edition(int edition) {
+            this.edition = edition;
+            return this;
+        }
+
+        public Book build(){
+            return new Book(title, firstNameAuthor, lastNameAuthor, year, edition);
+        }
+
+
+    }
+
+
+}
