4a277bc9cc36225153abb94f5b18ba74f1d2bb57
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-01 11:18:40 -0300

    feat: added new options for menu

--- src/main/java/com/homelib/Main.java
+++ src/main/java/com/homelib/Main.java
@@ -3,6 +3,7 @@ package com.homelib;
 import com.homelib.entities.Book;
 import com.homelib.entities.GlobalStore;
 import com.homelib.repository.BookRepository;
+import com.homelib.service.BookService;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Scanner;
@@ -13,13 +14,24 @@ public class Main {
 
     public static void main(String[] args) {
         int menuOption;
+        Book book = Book.BookBuilder
+                .builder()
+                .title("Dune")
+                .FirstNameAuthor("Frank")
+                .LastNameAuthor("Herbert")
+                .Year(1966)
+                .Edition(1)
+                .build();
+        BookService.save(book);
+        System.out.println(book);
         while (true){
             mainMenu();
             menuOption = Integer.parseInt(SCANNER.nextLine());
             if(menuOption == 0){break;}
             switch (menuOption){
                 case 1 -> GlobalStore.getInstance().getData();
-                case 2 -> BookRepository.findAllBooks();
+                case 2 -> BookRepository.findAllBooks().toString();
+                case 3 -> BookRepository.findById(book.getId());
                 //default -> throw new IllegalArgumentException("Opção não existe");
             }
         }
@@ -27,7 +39,7 @@ public class Main {
     }
 
     private static void mainMenu(){
-        System.out.println("1 - Listar todos os livros salvos");
+        System.out.println("1. Listar todos os livros salvos");
         System.out.println("2. Listar todos os livros via service");
         System.out.println("0. Sair do programa");
     }
