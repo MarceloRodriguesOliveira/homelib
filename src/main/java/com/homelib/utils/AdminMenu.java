2466dbbb72bafea697ba5e3b22c4eb730c1bc0fd
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-05-08 17:35:50 -0300

    feat: created menus to be implemented - not finished yet

---
+++ src/main/java/com/homelib/utils/AdminMenu.java
@@ -0,0 +1,8 @@
+package com.homelib.utils;
+
+public class AdminMenu implements Menu{
+    @Override
+    public void options() {
+        System.out.println("Apagar entradas");
+    }
+}
---
+++ src/main/java/com/homelib/utils/ServiceMenu.java
@@ -0,0 +1,8 @@
+package com.homelib.utils;
+
+public class ServiceMenu implements Menu {
+    @Override
+    public void options() {
+        System.out.println("1.");
+    }
+}
