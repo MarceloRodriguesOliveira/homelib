3fb16b9a46bb94ff642edd4db65443396d3cb857
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-08 17:36:13 -0300

    feat: added new options for insertion and refactored the main menu portion

--- src/main/java/com/homelib/Main.java
+++ src/main/java/com/homelib/Main.java
@@ -2,11 +2,15 @@ package com.homelib;
 
 import com.homelib.entities.Book;
 import com.homelib.entities.GlobalStore;
+import com.homelib.enums.MenuType;
+import com.homelib.factory.MenuFactory;
 import com.homelib.repository.BookRepository;
 import com.homelib.service.BookService;
+import com.homelib.utils.Menu;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 @Log4j2
 public class Main {
@@ -14,33 +18,29 @@ public class Main {
 
     public static void main(String[] args) {
         int menuOption;
+        Menu menu = MenuFactory.getMenu(MenuType.MAIN);
+        UUID uuid = UUID.randomUUID();
         Book book = Book.BookBuilder
                 .builder()
                 .title("Dune")
-                .FirstNameAuthor("Frank")
-                .LastNameAuthor("Herbert")
-                .Year(1966)
-                .Edition(1)
+                .firstNameAuthor("Frank")
+                .lastNameAuthor("Herbert")
+                .year(1966)
+                .edition(1)
                 .build();
         BookService.save(book);
-        System.out.println(book);
+        //System.out.println(book);
         while (true){
-            mainMenu();
+            menu.options();
             menuOption = Integer.parseInt(SCANNER.nextLine());
             if(menuOption == 0){break;}
             switch (menuOption){
-                case 1 -> GlobalStore.getInstance().getData();
-                case 2 -> BookRepository.findAllBooks().toString();
-                case 3 -> BookRepository.findById(book.getId());
-                //default -> throw new IllegalArgumentException("Opção não existe");
+                case 1 -> BookRepository.findAllBooks().toString();
+                case 2 -> BookService.findById(book.getId());
+                case 3 -> BookService.createNewBook();
             }
         }
 
     }
 
-    private static void mainMenu(){
-        System.out.println("1. Listar todos os livros salvos");
-        System.out.println("2. Listar todos os livros via service");
-        System.out.println("0. Sair do programa");
-    }
 }
