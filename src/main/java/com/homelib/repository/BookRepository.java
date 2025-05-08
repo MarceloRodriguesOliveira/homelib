11709ca886b9f13f97a2f17676a94204b1b78d7d
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-08 17:35:21 -0300

    chore: new method implementation for book builder

--- src/main/java/com/homelib/repository/BookRepository.java
+++ src/main/java/com/homelib/repository/BookRepository.java
@@ -6,6 +6,7 @@ import com.homelib.entities.GlobalStore;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Log4j2
@@ -19,14 +20,27 @@ public class BookRepository {
         return GlobalStore.getInstance().getData();
     }
 
-    public static void findById(UUID targetID){
+    public static Optional<Book> findById(UUID targetID){
         List<Book> books = GlobalStore.getInstance().getData();
+        //Book targetBook;
         for (Book book:books){
             if(book.getId().equals(targetID)){
+                Book targetBook = Book.BookBuilder
+                        .builder()
+                        .title(book.getTitle())
+                        .firstNameAuthor(book.getFirstNameAuthor())
+                        .lastNameAuthor(book.getLastNameAuthor())
+                        .year(book.getYear())
+                        .edition(book.getEdition())
+                        .id(book.getId())
+                        .build();
                 System.out.println(book);
+                return Optional.of(targetBook);
             }
         }
+        return Optional.empty();
     }
+
 
 
 
