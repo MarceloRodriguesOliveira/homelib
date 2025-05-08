d994a2c1e957ce92905769690af15770b84fb394
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-08 17:32:53 -0300

    feat: created factory for menu and main menu options

---
+++ src/main/java/com/homelib/factory/MenuFactory.java
@@ -0,0 +1,25 @@
+package com.homelib.factory;
+
+import com.homelib.enums.MenuType;
+import com.homelib.utils.AdminMenu;
+import com.homelib.utils.MainMenu;
+import com.homelib.utils.Menu;
+import com.homelib.utils.ServiceMenu;
+
+public class MenuFactory  {
+    public static Menu getMenu(MenuType menu){
+        switch (menu){
+            case MAIN -> {
+                return new MainMenu();
+            }
+            case ADMIN -> {
+                return new AdminMenu();
+            }
+            case SERVICE_OPERATIONS -> {
+                return new ServiceMenu();
+            }
+            default -> throw new IllegalArgumentException("Menu não encontrado");
+        }
+
+    }
+}
