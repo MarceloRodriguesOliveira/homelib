ae5ba2e906226704fe6ffd81db2c6a80c0e8f62d
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-05-08 17:34:35 -0300

    feat: created new method for insertion of book data

--- src/main/java/com/homelib/service/BookService.java
+++ src/main/java/com/homelib/service/BookService.java
@@ -3,12 +3,17 @@ package com.homelib.service;
 import com.homelib.entities.Book;
 import com.homelib.entities.GlobalStore;
 import com.homelib.repository.BookRepository;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 import java.util.UUID;
 
+@Log4j2
 public class BookService {
+    private static final Scanner SCANNER = new Scanner(System.in);
     public static void save(Book book){
         BookRepository.saveToDatabase(book);
     }
@@ -17,6 +22,42 @@ public class BookService {
         List<Book> bookList = new ArrayList<>(BookRepository.findAllBooks());
         return bookList;
     }
+
+    public static void findById(UUID id){
+        Optional<Book> bookFromDb = BookRepository.findById(id);
+        if(bookFromDb.isEmpty()){
+            System.out.println("No such identifier");
+        }
+
+        System.out.println(bookFromDb.toString());
+    }
+
+    public static void createNewBook(){
+        System.out.println("Type the title of the book: ");
+        String title = SCANNER.nextLine();
+        System.out.println("Type author's first name: ");
+        String firstName = SCANNER.nextLine();
+        System.out.println("Type author's last name: ");
+        String lastName = SCANNER.nextLine();
+        System.out.println("Type the year of publication");
+        Integer year = Integer.parseInt(SCANNER.nextLine());
+        System.out.println("Type the edition number: ");
+        Integer edition = Integer.parseInt(SCANNER.nextLine());
+
+        Book createBook = Book
+                .BookBuilder
+                .builder()
+                .title(title)
+                .firstNameAuthor(firstName)
+                .lastNameAuthor(lastName)
+                .year(year)
+                .edition(edition)
+                .build();
+        log.info("saving '{}' by {}, {} ", title, lastName, firstName );
+        System.out.println(createBook.toString());
+        BookRepository.saveToDatabase(createBook);
+    }
+
 
 
 }
