03434764f5190dc42993b2fa756f916dd9e713f6
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-15 15:37:22 -0300

    chore: created interface for Scanner/System.in operations

---
+++ src/main/java/com/homelib/utils/BookInputReader.java
@@ -0,0 +1,41 @@
+package com.homelib.utils;
+
+import com.homelib.entities.Book;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public interface BookInputReader {
+    static final Scanner SCANNER = new Scanner(System.in);
+
+    default Book readBook(){
+        System.out.println("Type the title of the book: ");
+        String title = SCANNER.nextLine();
+        System.out.println("Type author's first name: ");
+        String firstName = SCANNER.nextLine();
+        System.out.println("Type author's last name: ");
+        String lastName = SCANNER.nextLine();
+        System.out.println("Type the year of publication");
+        Integer year = Integer.parseInt(SCANNER.nextLine());
+        System.out.println("Type the edition number: ");
+        Integer edition = Integer.parseInt(SCANNER.nextLine());
+
+        Book createBook = Book
+                .BookBuilder
+                .builder()
+                .title(title)
+                .firstNameAuthor(firstName)
+                .lastNameAuthor(lastName)
+                .year(year)
+                .edition(edition)
+                .build();
+        return createBook;
+
+    }
+
+    default int readId(){
+        System.out.println("Type the id of the book: ");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+}
