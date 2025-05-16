22af424e425988dfc5900885bc0d993f906cbb94
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-15 23:39:29 -0300

    feat: some services methods are not called directly anymore

--- src/main/java/com/homelib/Main.java
+++ src/main/java/com/homelib/Main.java
@@ -1,16 +1,18 @@
 package com.homelib;
 
-import com.homelib.entities.Book;
-import com.homelib.entities.GlobalStore;
+
 import com.homelib.enums.MenuType;
+import com.homelib.enums.OperationType;
+import com.homelib.factory.BookInputFactory;
 import com.homelib.factory.MenuFactory;
-import com.homelib.repository.BookRepository;
+
 import com.homelib.service.BookService;
+import com.homelib.utils.BookOperations;
 import com.homelib.utils.Menu;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Scanner;
-import java.util.UUID;
+
 
 @Log4j2
 public class Main {
@@ -19,24 +21,14 @@ public class Main {
     public static void main(String[] args) {
         int menuOption;
         Menu menu = MenuFactory.getMenu(MenuType.MAIN);
-        UUID uuid = UUID.randomUUID();
-        Book book = Book.BookBuilder
-                .builder()
-                .title("Dune")
-                .firstNameAuthor("Frank")
-                .lastNameAuthor("Herbert")
-                .year(1966)
-                .edition(1)
-                .build();
-        BookService.save(book);
         while (true){
             menu.options();
             menuOption = Integer.parseInt(SCANNER.nextLine());
             if(menuOption == 0){break;}
             switch (menuOption){
-                case 1 -> BookService.findAll();
-                case 2 -> BookService.findById();
-                case 3 -> BookService.createNewBook();
+                case 1 -> BookInputFactory.getMenu(OperationType.LIST_BOOKS);
+                case 2 -> BookInputFactory.getMenu(OperationType.SEARCH_ID);
+                case 3 -> BookInputFactory.getMenu(OperationType.CREATE);
                 case 4 -> BookService.deleteBookById();
             }
         }
