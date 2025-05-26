9a702472b505be14798b9875a73229bb78ec1c71
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-26 15:08:04 -0300

    chore: renamed file selection class

--- src/main/java/com/homelib/output/io/FileReaderHelper.java
+++ src/main/java/com/homelib/output/io/FileReaderHelper.java
@@ -2,14 +2,13 @@ package com.homelib.output.io;
 
 import javax.swing.*;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReaderHelper {
     public static void readFromFile(){
-        JFileChooser chooser = FileSelector.selectFile();
+        JFileChooser chooser = FileSelectorHelper.selectFile();
         int returnVal = chooser.showOpenDialog(null);
         if(returnVal == JFileChooser.APPROVE_OPTION){
             try(BufferedReader reader = new BufferedReader(new FileReader(chooser.getSelectedFile()))){
--- src/main/java/com/homelib/output/io/FileSelectorHelper.java
+++ src/main/java/com/homelib/output/io/FileSelectorHelper.java
@@ -2,12 +2,9 @@ package com.homelib.output.io;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
 
-public class FileSelector {
-    private FileSelector(){}
+public class FileSelectorHelper {
+    private FileSelectorHelper(){}
 
     public static JFileChooser selectFile(){
         JFileChooser fileChooser = new JFileChooser();
