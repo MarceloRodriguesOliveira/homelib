4ae89e4c34b20f45f35a241b8478ab7275ff5b34
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-26 18:27:25 -0300

    fea: created method that export data as csv

--- src/main/java/com/homelib/output/io/OutputFileWriterHelper.java
+++ src/main/java/com/homelib/output/io/OutputFileWriterHelper.java
@@ -6,6 +6,7 @@ import com.homelib.output.formatter.BookFormatter;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -24,6 +25,23 @@ public class OutputFileWriterHelper {
         }catch (IOException e){
             System.out.println("Could not write file");
 
+        }
+    }
+
+    public static void exportCsv(List<Book> books){
+        JFileChooser chooser = FileSelectorHelper.selectFile();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = chooser.showOpenDialog(null);
+        if(returnVal == JFileChooser.APPROVE_OPTION){
+            String fullPath = chooser.getSelectedFile().getAbsolutePath() + File.separator + "exportedList.csv";
+            try(FileWriter writer = new FileWriter(fullPath)){
+                for(Book book: books){
+                    String csvString = BookFormatter.formatAsCsv(book);
+                    writer.write(csvString + "\n");
+                }
+            }catch (IOException e){
+                System.out.println("Could not proceed with operation");
+            }
         }
     }
 
