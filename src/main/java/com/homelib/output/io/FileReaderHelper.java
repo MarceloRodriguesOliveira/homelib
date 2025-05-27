e9ba4cbb5385969524e6a29dfc80671defe51cb5
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-26 22:23:02 -0300

    feat: created method for csv reading

--- src/main/java/com/homelib/output/io/FileReaderHelper.java
+++ src/main/java/com/homelib/output/io/FileReaderHelper.java
@@ -1,10 +1,15 @@
 package com.homelib.output.io;
 
+import com.homelib.entities.Book;
+import com.homelib.output.formatter.BookFormatter;
+
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileReaderHelper {
     public static void readFromFile(){
@@ -23,7 +28,28 @@ public class FileReaderHelper {
                 System.out.println("Error while accessing file");
             }
         }
+    }
 
+    public static void readListFromCsv(){
+        List<Book> bookList_CSV = new ArrayList<Book>();
+        JFileChooser chooser = FileSelectorHelper.selectFile();
+        int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION){
+            try(BufferedReader reader = new BufferedReader(new FileReader(chooser.getSelectedFile()))){
+                String line;
+                while ((line = reader.readLine()) != null){
+                    bookList_CSV.add(BookFormatter.getCsvValueAsBook(line));
+                }
+                BookFormatter.formattedBookListFromCsv(bookList_CSV);
+                return;
+            }catch (FileNotFoundException e){
+                System.out.println("File not found");
+
+            }catch (IOException e){
+                System.out.println("Error while acessing file");
+
+            }
+        }
 
     }
 }
