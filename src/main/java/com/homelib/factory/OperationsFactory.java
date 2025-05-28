63ae085fc6db119edb055610bc6cdd7f8db8d50d
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-28 11:17:38 -0300

    chore: consoleBookInputReader will now be injected with other main classes

--- src/main/java/com/homelib/factory/OperationsFactory.java
+++ src/main/java/com/homelib/factory/OperationsFactory.java
@@ -5,7 +5,11 @@ import com.homelib.output.io.FileSelectorHelper;
 import com.homelib.output.io.OutputFileWriterHelper;
 import com.homelib.repository.BookRepository;
 import com.homelib.service.BookService;
+import com.homelib.utils.BookInputReader;
 import com.homelib.utils.BookOperations;
+import com.homelib.utils.ConsoleBookInputReader;
+
+import java.util.Scanner;
 
 public class OperationsFactory {
     public static BookOperations createBookOperations(){
@@ -15,7 +19,10 @@ public class OperationsFactory {
         FileSelectorHelper fileSelectorHelper = new FileSelectorHelper();
         FileReaderHelper fileReaderHelper = new FileReaderHelper(fileSelectorHelper);
         OutputFileWriterHelper outputFileWriterHelper = new OutputFileWriterHelper(fileSelectorHelper);
-        return new BookOperations(bookService, fileReaderHelper, outputFileWriterHelper);
+
+        Scanner scanner = new Scanner(System.in);
+        BookInputReader bookInputReader = new ConsoleBookInputReader(scanner);
+        return new BookOperations(bookService, fileReaderHelper, outputFileWriterHelper, bookInputReader);
 
     }
 
