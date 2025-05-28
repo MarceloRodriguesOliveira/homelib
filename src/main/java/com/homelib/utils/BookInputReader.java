f974e87bc9cb2663f9c97df197aed6362a55e072
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-05-28 11:15:02 -0300

    chore: removed logic from inteface

--- src/main/java/com/homelib/utils/BookInputReader.java
+++ src/main/java/com/homelib/utils/BookInputReader.java
@@ -7,50 +7,9 @@ import java.util.Optional;
 import java.util.Scanner;
 
 public interface BookInputReader {
-    static final Scanner SCANNER = new Scanner(System.in);
-
-    default Book readBook(){
-        System.out.println("Type the title of the book: ");
-        String title = SCANNER.nextLine();
-        System.out.println("Type author's first name: ");
-        String firstName = SCANNER.nextLine();
-        System.out.println("Type author's last name: ");
-        String lastName = SCANNER.nextLine();
-        System.out.println("Type the year of publication");
-        Integer year = Integer.parseInt(SCANNER.nextLine());
-        System.out.println("Type the edition number: ");
-        Integer edition = Integer.parseInt(SCANNER.nextLine());
-
-        Book createBook = Book
-                .BookBuilder
-                .builder()
-                .title(title)
-                .firstNameAuthor(firstName)
-                .lastNameAuthor(lastName)
-                .year(year)
-                .edition(edition)
-                .build();
-        return createBook;
-
-    }
-
-    default int readId(){
-        System.out.println("Type the id of the book: ");
-        return Integer.parseInt(SCANNER.nextLine());
-    }
-
-    default String listBooks(){
-        System.out.println("Digite o título do livro ou deixe em branco para listar todos: ");
-        return SCANNER.nextLine();
-    }
-
-    default int readIdDelete(){
-        System.out.println("Digite o id do livro que deseja apagar");
-        return Integer.parseInt(SCANNER.nextLine());
-    }
-
-    default int readIdUpdate(){
-        System.out.println("Digite a id do livro que deseja atualizar");
-        return Integer.parseInt(SCANNER.nextLine());
-    }
+    Book readBook();
+    int readId();
+    String listBooks();
+    int readIdDelete();
+    int readIdUpdate();
 }
