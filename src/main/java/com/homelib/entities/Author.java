400f535183c09d10c0025e8f0cdc6b233768d08b
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-06-16 14:16:37 -0300

    feat: created new entity for author mapping

---
+++ src/main/java/com/homelib/entities/Author.java
@@ -0,0 +1,34 @@
+package com.homelib.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Author {
+    private String firstName;
+    private String lastName;
+    private Long id;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Long id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+
+}
