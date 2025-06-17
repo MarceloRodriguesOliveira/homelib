8b7b4f768128fc4f5e3dd110c24ea78032356e15
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-06-16 23:09:37 -0300

    feat: insert operations are now functional

--- src/main/java/com/homelib/repository/AuthorRepository.java
+++ src/main/java/com/homelib/repository/AuthorRepository.java
@@ -1,6 +1,9 @@
 package com.homelib.repository;
 
+import com.homelib.connection.ConnectionFactory;
 import com.homelib.entities.Author;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,8 +12,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AuthorRepository {
+
+    private static final Logger log = LogManager.getLogger(AuthorRepository.class);
 
     public List<Long> SaveAuthors(Connection conn, List<Author> authors) throws SQLException {
         if(authors.size() == 1 ){
@@ -23,8 +29,17 @@ public class AuthorRepository {
     public List<Long> saveAuthorsBatch(Connection conn, List<Author> authors) throws SQLException {
         if(authors == null || authors.isEmpty()) return List.of();
         List<Long> generatedIds = new ArrayList<>();
+        List<Author> authorsToInsert = new ArrayList<>();
+
+        for (Author author:authors){
+            Optional<Long> existingId = findAuthorByName(conn, author);
+            if(existingId.isPresent()){
+                generatedIds.add(existingId.get());
+            }else authorsToInsert.add(author);
+        }
+
         try(PreparedStatement ps = createPreparedStatementSaveAuthorBatch(conn)){
-            for (Author author: authors){
+            for (Author author: authorsToInsert){
                 ps.setString(1, author.getFirstName());
                 ps.setString(2, author.getLastName());
                 ps.addBatch();
@@ -32,7 +47,7 @@ public class AuthorRepository {
             ps.executeBatch();
 
             try(ResultSet rs = ps.getGeneratedKeys()){
-                if(rs.next()){
+                while (rs.next()){
                     generatedIds.add(rs.getLong(1));
                 }
             }
@@ -41,6 +56,11 @@ public class AuthorRepository {
     }
 
     public Long saveSingleAuthor(Connection conn, Author author) throws SQLException {
+        Optional<Long> existingId = findAuthorByName(conn, author);
+        if(existingId.isPresent()){
+            log.info("Author already exists on database. Aborting insert...");
+            return existingId.get();
+        }
         try(PreparedStatement ps = createPreparedStatementSaveSingleAuthor(conn, author)){
             ps.executeUpdate();
             try(ResultSet rs = ps.getGeneratedKeys()){
@@ -53,8 +73,19 @@ public class AuthorRepository {
     }
 
 
+    public Optional<Long> findAuthorByName(Connection conn, Author author){
+        try(PreparedStatement ps = createPreparedStatementFindAuthorByName(conn, author);
+            ResultSet rs = ps.executeQuery()){
+            if(!rs.next()) return Optional.empty();
+            return Optional.of(rs.getLong(1));
+        }catch (SQLException e){
+            log.error("Could not retrieve info on author");
+        }
+        return Optional.empty();
+    }
+
     private static PreparedStatement createPreparedStatementSaveSingleAuthor(Connection conn, Author author) throws SQLException {
-        String sql = "INSERT INTO author_store (firstname, lastname) VALUES (?, ?)";
+        String sql = "INSERT INTO author_store (first_name, last_name) VALUES (?, ?)";
         PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
         ps.setString(1, author.getFirstName());
         ps.setString(2, author.getLastName());
@@ -64,6 +95,14 @@ public class AuthorRepository {
     private static PreparedStatement createPreparedStatementSaveAuthorBatch(Connection conn) throws SQLException{
         String sql = "INSERT INTO author_store (first_name, last_name) VALUES (?, ?)";
         return conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+    }
+
+    private static PreparedStatement createPreparedStatementFindAuthorByName(Connection conn, Author author) throws SQLException{
+        String sql = "SELECT id, first_name, last_name FROM author_store WHERE first_name = ? AND last_name = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, author.getFirstName());
+        ps.setString(2, author.getLastName());
+        return ps;
     }
 
 }
--- src/main/java/com/homelib/repository/BookRepository.java
+++ src/main/java/com/homelib/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.homelib.repository;
 
 
 import com.homelib.connection.ConnectionFactory;
+import com.homelib.entities.Author;
 import com.homelib.entities.Book;
 import lombok.extern.log4j.Log4j2;
 
@@ -10,19 +11,24 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Log4j2
 public class BookRepository {
+    AuthorRepository authorRepository = new AuthorRepository();
+    BookAuthorRepository bookAuthorRepository = new BookAuthorRepository();
     public Book save(Book book){
         try(Connection conn = ConnectionFactory.getConnection(); PreparedStatement ps = createPrepareStatementSave(conn, book);) {
             ps.execute();
+            authorRepository.SaveAuthors(conn, book.getAuthors());
 
             try( ResultSet rs = ps.getGeneratedKeys();){
                 if(rs.next()){
-                    book.setId(rs.getInt(1));
+                    book.setId(rs.getLong(1));
                 }
             }
             return book;
@@ -31,19 +37,34 @@ public class BookRepository {
         }
     }
 
+    public Long saveSingleBook(Connection conn, Book book) throws SQLException{
+        try(PreparedStatement ps = createPrepareStatementSave(conn, book)){
+            try(ResultSet rs = ps.getGeneratedKeys()){
+                if(rs.next()) return rs.getLong(1);
+            }
+        }
+        throw new SQLException("Failed to save book");
+    }
+
     private static PreparedStatement createPrepareStatementSave(Connection conn, Book book ) throws SQLException {
-        String sql = "INSERT INTO `book_store` (`title`, `firstnameauthor`, `lastnameauthor`, `year`, `edition`) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO `book_store` (`title`, `year`, `edition`, `publisher`, `locale`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
         ps.setString(1, book.getTitle());
-        ps.setString(2, book.getFirstNameAuthor());
-        ps.setString(3, book.getLastNameAuthor());
-        ps.setInt(4, book.getYear());
-        ps.setInt(5, book.getEdition());
+        ps.setInt(2, book.getYear());
+        ps.setInt(3, book.getEdition());
+        ps.setString(4, book.getPublisher());
+        ps.setString(5, book.getLocale().toString());
+
+        LocalDateTime now = LocalDateTime.now();
+        ps.setTimestamp(6, Timestamp.valueOf(now));
+        ps.setTimestamp(7, Timestamp.valueOf(now));
+
         return ps;
     }
 
     public List<Book> findAllBooks(String title){
         List<Book> booksFound = new ArrayList<Book>();
+        List<Author> bookAuthors = new ArrayList<>();
         try(Connection conn = ConnectionFactory.getConnection();
             PreparedStatement ps = createPreparedStatementFindByTitle(conn, title);
             ResultSet rs = ps.executeQuery()){
@@ -51,11 +72,10 @@ public class BookRepository {
                 Book book = Book.BookBuilder
                         .builder()
                         .title(rs.getString("title"))
-                        .firstNameAuthor(rs.getString("firstnameauthor"))
-                        .lastNameAuthor(rs.getString("lastnameauthor"))
+                        .authors(List.of(new Author(rs.getString("firstnameauthor"), rs.getString("lastnameauthor"))))
                         .year(rs.getInt("year"))
                         .edition(rs.getInt("edition"))
-                        .id(rs.getInt("id"))
+                        .id(rs.getLong("id"))
                         .build();
                 booksFound.add(book);
             }
@@ -67,7 +87,7 @@ public class BookRepository {
 
     public static PreparedStatement createPreparedStatementFindByTitle(Connection conn, String title) throws SQLException {
         String sql = """
-                     SELECT title, firstnameauthor, lastnameauthor, year, edition, id FROM book_store WHERE title LIKE ?;
+                     SELECT title, year, edition, id, publisher, locale FROM book_store WHERE title LIKE ?;
                      """;
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, String.format("%%%s%%", title));
@@ -76,7 +96,7 @@ public class BookRepository {
 
 
 
-    public Optional<Book> findById(int targetID){
+    public Optional<Book> findById(Long targetID){
         try(Connection conn = ConnectionFactory.getConnection();
             PreparedStatement ps = createPreparedStatementFindById(conn, targetID);
             ResultSet rs = ps.executeQuery();
@@ -85,11 +105,10 @@ public class BookRepository {
             return Optional.of(Book.BookBuilder
                     .builder()
                     .title(rs.getString("title"))
-                    .firstNameAuthor(rs.getString("firstnameauthor"))
-                    .lastNameAuthor(rs.getString("lastnameauthor"))
+                    .authors(List.of(new Author(rs.getString("firstnameauthor"), rs.getString("lastnameauthor"))))
                     .year(rs.getInt("year"))
                     .edition(rs.getInt("edition"))
-                    .id(rs.getInt("id"))
+                    .id(rs.getLong("id"))
                     .build());
         }catch (SQLException e){
             log.error("Error while searching for this id");
@@ -97,14 +116,14 @@ public class BookRepository {
         return Optional.empty();
     }
 
-    public static PreparedStatement createPreparedStatementFindById(Connection conn, int id) throws SQLException{
+    public static PreparedStatement createPreparedStatementFindById(Connection conn, Long id) throws SQLException{
         String sql = "SELECT * FROM `book_store` WHERE id = ?";
         PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        ps.setInt(1, id);
+        ps.setLong(1, id);
         return ps;
     }
 
-    public void deleteBookById(int id){
+    public void deleteBookById(Long id){
        try(Connection conn = ConnectionFactory.getConnection();
            PreparedStatement ps = createPreparedStatementDeleteById(conn, id)
           ){
@@ -116,10 +135,10 @@ public class BookRepository {
 
     }
 
-    public static PreparedStatement createPreparedStatementDeleteById(Connection conn, int id) throws SQLException{
+    public static PreparedStatement createPreparedStatementDeleteById(Connection conn, Long id) throws SQLException{
         String sql = "DELETE FROM `book_store` WHERE (`id` = ?)";
         PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setInt(1, id);
+        ps.setLong(1, id);
         return ps;
     }
 
@@ -142,7 +161,7 @@ public class BookRepository {
         ps.setString(3, book.getLastNameAuthor());
         ps.setInt(4, book.getYear());
         ps.setInt(5, book.getEdition());
-        ps.setInt(6, book.getId());
+        ps.setLong(6, book.getId());
 
         return ps;
     }
