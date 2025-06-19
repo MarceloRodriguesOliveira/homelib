46164f1e7e31d3ee9c7f8942b191d224e31b00c3
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-06-18 21:15:47 -0300

    feat: created method for book-author query

--- src/main/java/com/homelib/repository/BookAuthorRepository.java
+++ src/main/java/com/homelib/repository/BookAuthorRepository.java
@@ -6,7 +6,9 @@ import com.homelib.entities.Book;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookAuthorRepository {
@@ -24,9 +26,29 @@ public class BookAuthorRepository {
         }
     }
 
+    public List<Long> findAuthorByBookId(Connection conn, long bookId){
+        List<Long> bookAuthorIds = new ArrayList<>();
+        try (PreparedStatement ps = createPreparedStatementFindAuthorByBookId(conn, bookId);
+             ResultSet rs = ps.executeQuery()){
+            while (rs.next()){
+                bookAuthorIds.add(rs.getLong("author_id"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return bookAuthorIds;
+    }
+
     private PreparedStatement createPreparedStatementBookAuthorLink(Connection conn) throws SQLException {
         String sql = "INSERT INTO book_author (book_id, author_id) VALUES (?, ?)";
         return conn.prepareStatement(sql);
+    }
+
+    private PreparedStatement createPreparedStatementFindAuthorByBookId(Connection conn, long bookId) throws SQLException {
+        String sql = "SELECT author_id FROM book_author WHERE book_id = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setLong(1, bookId);
+        return ps;
     }
 
 }
