7453d8e0eac7b7221c8c0dd4236aceb864e211b9
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-06-25 11:21:34 -0300

    feat: reading from csv files is now functional

--- src/main/java/com/homelib/output/formatter/BookFormatter.java
+++ src/main/java/com/homelib/output/formatter/BookFormatter.java
@@ -2,11 +2,17 @@ package com.homelib.output.formatter;
 
 import com.homelib.entities.Author;
 import com.homelib.entities.Book;
+import com.homelib.enums.PublisherLocale;
+import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+@Log4j2
 public class BookFormatter {
     private BookFormatter(){}
 
@@ -18,23 +24,45 @@ public class BookFormatter {
         return String.format("%s,%s,%s,%d ed,%d", book.getTitle(), book.getFirstNameAuthor(),book.getLastNameAuthor(), book.getEdition(), book.getYear());
     }*/
 
-    /*public static Book getCsvValueAsBook(String line){
+    public static Book getCsvValueAsBook(String line){
+        List<Author> authorListFromLine = new ArrayList<>();
         Pattern csvSplit = Pattern.compile(",");
+        Pattern splitNames = Pattern.compile(";");
         String[] fields = csvSplit.split(line);
+        String[] names = splitNames.split(fields[5]);
 
-        if(fields.length < 5  ){
-            throw new IllegalArgumentException("Formato da linha invalido "+ line);
-        }
+        Arrays.stream(names).map(a->{
+            int lastSpace = a.lastIndexOf(" ");
+            if(lastSpace == -1){
+                log.warn("Linha mal formatada {}, inserindo apenas primeiro nome", a);
+                return new Author(a.trim(), "");
+            }
+            String lastname = a.substring(lastSpace+1).trim();
+            String firstname = a.substring(0, lastSpace);
+            return new Author(firstname, lastname);
+        }).forEachOrdered(authorListFromLine::add);
+
+        /*for (String name : names) {
+            int lastSpace = name.lastIndexOf(" ");
+            if(lastSpace == -1){
+                throw new IllegalArgumentException("Linha mal formatada");
+            }
+            String lastname = name.substring(lastSpace+1).trim();
+            String firstname = name.substring(0, lastSpace).trim();
+            Author authorFromLine = new Author(firstname, lastname);
+            authorListFromLine.add(authorFromLine);
+        }*/
 
         return Book.BookBuilder
                 .builder()
                 .title(fields[0])
-                .firstNameAuthor(fields[1])
-                .lastNameAuthor(fields[2])
-                .edition(Integer.parseInt(fields[3].substring(0,1)))
-                .year(Integer.parseInt(fields[4]))
+                .authors(authorListFromLine)
+                .year(Integer.parseInt(fields[1]))
+                .edition(Integer.parseInt(fields[2]))
+                .publisher(fields[3])
+                .locale(PublisherLocale.valueOf(fields[4]))
                 .build();
-    }*/
+    }
 
     public static void formattedBookList(List<Book> books){
 
@@ -57,7 +85,7 @@ public class BookFormatter {
         System.out.printf("%s (%d) by %s.%s (%s)\n", book.getTitle(),book.getYear() ,book.getAuthorsFullName(book.getAuthors()), book.getPublisher(), book.getLocale());
     }
 
-    /*public static void formattedBookListFromCsv(List<Book> books){
+    public static void formattedBookListFromCsv(List<Book> books){
         System.out.print(
                 "+----------------------------+-------------------------------+------+---------+--------------------------------------+\n");
         System.out.printf(
@@ -66,11 +94,13 @@ public class BookFormatter {
         System.out.print(
                 "+----------------------------+-------------------------------+------+---------+--------------------------------------+\n");
         for (Book book: books){
-            System.out.printf("| %-26s | %-29s | %-4d | %-7s |\n", book.getTitle(), book.getAuthor(), book.getYear(), book.getEdition());
+            String authorNames = book.getAuthorsFullName(book.getAuthors());
+
+            System.out.printf("| %-26s | %-29s | %-4d | %-7s |\n", book.getTitle(), authorNames, book.getYear(), book.getEdition());
 
         }
         System.out.print("----------------------------------------------------------------------------------------------------------------------\n");
-    }*/
+    }
 
 
     /*public static void formattedImportedList(){
